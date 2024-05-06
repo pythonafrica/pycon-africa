561a791b36781d527694ae372c0487409e274d60
Mannie <mannieyoung7@gmail.com>
2024-05-06 04:22:27 +0000

    fix an issue in profile class

--- registration/models.py
+++ registration/models.py
@@ -810,8 +810,9 @@ class Profile(models.Model, HitCountMixin):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
+        Profile.objects.create(user=instance) 
+    instance.user_profile.save()
+
 
 
 
