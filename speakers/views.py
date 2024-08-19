c5c69726088f8a386b3409923330b61f9e3c9596
Mannie <mannieyoung7@gmail.com>
2024-08-19 21:48:58 +0000

    [ Update - metafixforsomepages]

--- speakers/views.py
+++ speakers/views.py
@@ -123,7 +123,7 @@ class SpeakerDetailView(HitCountDetailView):
         truncated_biography = Truncator(self.object.biography).words(30, truncate='...')
 
         # Meta tags information
-        meta_title = f"{self.object.name} | PyCon Africa {year}"
+        meta_title = f"{self.object.name} {self.object.surname} | PyCon Africa {year}"
         meta_description = f"Meet {self.object.name}, a speaker at PyCon Africa {year}. {truncated_biography}"
         meta_author = "PyCon Africa"
         meta_og_image = self.object.profile_image.url if self.object.profile_image else "https://res.cloudinary.com/pycon-africa/image/upload/v1722977619/website_storage_location/media/pyconafrica.png"
