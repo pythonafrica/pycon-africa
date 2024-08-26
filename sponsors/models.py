c740878ab6cdd441f71105ec612c4da373a8c446
Mannie <mannieyoung7@gmail.com>
2024-08-26 05:51:55 +0000

    [ Update - sponsors]

--- sponsors/models.py
+++ sponsors/models.py
@@ -16,28 +16,29 @@ class Sponsor(models.Model):
     name = models.CharField("sponsor name", max_length=200)
     tier = models.ForeignKey(SponsorshipTier, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="sponsorship tier")
     logo = models.ImageField(upload_to="sponsors/", max_length=255, blank=True, null=True)
-    sponsor_type = models.CharField("sponsor type", max_length=1, choices=SPONSOR_TYPE)   
+    sponsor_type = models.CharField("sponsor type", max_length=1, choices=SPONSOR_TYPE)
     is_visible = models.BooleanField(default=False)
-    website = models.URLField(default='', help_text='Link to Sponsor website', blank=True,) 
-    twitter = models.CharField(max_length=100, null=True, help_text="Please enter only the user name eg.'mawy_7'", default="", blank=True,)
-    linkedin = models.CharField(max_length=100, null=True, help_text="Please enter only the user name eg.'mawy_7'", default="", blank=True,) 
+    website = models.URLField(default='', help_text='Link to Sponsor website', blank=True)
+    twitter = models.CharField(max_length=100, null=True, help_text="Please enter only the user name eg.'mawy_7'", default="", blank=True)
+    linkedin = models.CharField(max_length=100, null=True, help_text="Please enter only the user name eg.'mawy_7'", default="", blank=True)
     biography = MarkdownxField(default='', help_text="Description of the Sponsor", blank=True, null=True)
-    show_biography = models.BooleanField(default=False, help_text="Untick if the company only want their logo displayed on our website. Not all companies want their information on the site.")
+    show_biography = models.BooleanField(default=False, help_text="Untick if the company only wants their logo displayed on our website. Not all companies want their information on the site.")
     event_year = models.ForeignKey(EventYear, on_delete=models.CASCADE, default="2024", related_name='sponsors')
     slug = models.SlugField(unique=True, blank=True)
 
     class Meta:
         managed = True
 
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
-        if not self.slug:
+        # Generate slug from name if not already set or if the name has changed
+        if not self.slug or slugify(self.name) != self.slug:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
     
     def get_absolute_url(self):
         return reverse('sponsor_detail', kwargs={'year': self.event_year.year, 'slug': self.slug})
- 
-
-
  
 
