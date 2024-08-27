648facfad4939481673205567a4f1bc1ac06c9db
Mannie <mannieyoung7@gmail.com>
2024-08-27 18:12:48 +0000

    [ Update - sponsors-admin]

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Sponsor
 from sponsor_us.models import SponsorshipTier
 from home.models import EventYear
 
 class SponsorAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_visible", "show_biography", "sponsor_type", "get_tier_display", "get_event_year_display")
+    list_display = ("logo_preview", "name", "is_visible", "show_biography", "sponsor_type", "get_tier_display", "get_event_year_display")
     list_editable = ["is_visible", "show_biography"]
     list_filter = ["is_visible", "sponsor_type", "tier__name", "event_year__year"]
     search_fields = ['name', 'biography']
@@ -26,4 +27,14 @@ class SponsorAdmin(admin.ModelAdmin):
         return obj.event_year.year if obj.event_year else '-'
     get_event_year_display.short_description = 'Event Year'
 
+    # Method to show a preview of the logo
+    def logo_preview(self, obj):
+        if obj.logo:
+            return format_html('<img src="{}" style="width: 45px; height: 45px;" />', obj.logo.url)
+        return "No Logo"
+    logo_preview.short_description = 'Logo Preview'
+
 admin.site.register(Sponsor, SponsorAdmin)
+
+
+ 
